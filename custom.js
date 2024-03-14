71efeea1b22da2293cc5832a1b39b1da7e3fea87
Jonnelpallado22 <jonnelpallado22@gmail.com>
2024-03-14 22:17:10 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,13 +8,13 @@ module.exports = async ({ api }) => {
 
   const config = {
     autoRestart: {
-      status: false,
-      time: 40,
+      status: true,
+      time: 10,
       note: 'To avoid problems, enable periodic bot restarts',
     },
     acceptPending: {
       status: false,
-      time: 30,
+      time: 10,
       note: 'Approve waiting messages after a certain time',
     },
   };
@@ -60,7 +60,7 @@ module.exports = async ({ api }) => {
       async function message(thread) {
         try {
           api.sendMessage({
-            body: `⟩ Thank you for using BotPack!\n\n⟩ Fork Here: https://replit.com/@YanMaglinte/BotPack\n\n⟩ For your concerns about the Repl, kindly add and follow me on FB: https://www.facebook.com/yandeva.me?mibextid=ZbWKwL`
+            body: ``
           }, thread.threadID, (err) => {
             if (err) return;
             messagedThreads.add(thread.threadID);
@@ -103,7 +103,7 @@ module.exports = async ({ api }) => {
       async function message(thread) {
         try {
           api.sendMessage({
-            body: `Hey There! How are you? ヾ(＾-＾)ノ`
+            body: ``
           }, thread.threadID, (err) => {
             if (err) return;
             messagedThreads.add(thread.threadID);
